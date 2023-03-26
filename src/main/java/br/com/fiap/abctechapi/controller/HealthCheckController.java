b7c3e6554fbccba8eb2ab24dd2a599f3f9792b8f
Alex Braga <alexbraga101@gmail.com>
2023-03-26 00:49:51 -0300

    Aula 1 - Criar Classe VersionComponent

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.abctechapi.controller;
 
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,16 @@ import java.util.Properties;
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
+    private VersionComponent versionComponent;
+    public HealthCheckController(@Autowired VersionComponent versionComponent){
+        this.versionComponent = versionComponent;
+    }
     @GetMapping
     public ResponseEntity<String> status(){
         return ResponseEntity.ok("UP");
     }
     @GetMapping("version")
-    public ResponseEntity<String> version() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-        properties.load(inputStream);
-        return ResponseEntity.ok(properties.get("build.name") + "-" + properties.get("build.version"));
+    public ResponseEntity<String> version(){
+        return ResponseEntity.ok(this.versionComponent.getVersion());
     }
 }
