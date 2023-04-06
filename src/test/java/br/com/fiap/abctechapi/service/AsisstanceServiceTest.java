d4ffab8c47b780136dc4d1ae54f96602ac296b3f
Alex Braga <alexbraga101@gmail.com>
2023-04-06 18:11:16 -0300

    Aula 2 - Adicionar endpoint /assists retornando dados do banco

--- src/test/java/br/com/fiap/abctechapi/service/AsisstanceServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/AsisstanceServiceTest.java
@@ -1,5 +1,7 @@
 package br.com.fiap.abctechapi.service;
 import br.com.fiap.abctechapi.entity.Assistance;
+import br.com.fiap.abctechapi.repository.AssistanceRepository;
+import br.com.fiap.abctechapi.service.impl.AssistanceServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,15 +15,18 @@ import java.util.List;
 @SpringBootTest
 
 public class AsisstanceServiceTest {
-    @MockBean
+
     private AssistanceService assistanceService;
+    @MockBean
+    private AssistanceRepository assistanceRepository;
     @BeforeEach
     public void init(){
         MockitoAnnotations.openMocks(this);
+        assistanceService = new AssistanceServiceImpl(assistanceRepository);
     }
     @Test
-    public void test_lista_assistance() {
-        Mockito.when(assistanceService.getAssists()).thenReturn(List.of(new Assistance(1L, "Test", "Test Description")));
+    public void test_list_assistance() {
+        Mockito.when(assistanceRepository.findAll()).thenReturn(List.of(new Assistance(1L, "Test", "Test Description")));
         List<Assistance> list = assistanceService.getAssists();
         Assertions.assertNotNull(list);
         Assertions.assertEquals(1, list.size());
